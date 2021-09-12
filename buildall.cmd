a00cf8e7185b60fc268b42e0d9ff2347472b417a
dbjdbj <dbj@dbj.org>
2021-09-12 17:54:49 +0200

    added

---
+++ buildall.cmd
@@ -0,0 +1,26 @@
+@cls
+@echo off
+
+pushd .
+pushd .\A
+call .\build-a.cmd
+popd
+popd
+
+pushd .
+pushd .\dbj-shmem
+call .\build-dbj-shmem.cmd
+popd
+popd
+
+pushd .
+pushd .\dbj-vector
+call .\build-dbj-vector.cmd
+popd
+popd
+
+pushd .
+pushd .\host_console_app
+call .\build-console-host.cmd
+popd
+popd
