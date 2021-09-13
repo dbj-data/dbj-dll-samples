c9bb99b7b444294242abb5cbd3f8122923d04240
dbjdbj <dbj@dbj.org>
2021-09-13 18:12:56 +0200

    faster and more resilient

--- dbj-syserrmsg/dbj-syserrmsg.c
+++ dbj-syserrmsg/dbj-syserrmsg.c
@@ -20,21 +20,20 @@ static HMODULE netmsg_handle_ = NULL;
 static bool also_dump_to_stderr_ = false;
 
 static dbj_string_512
-DisplayErrorText(
+MakeErrorText(
     DWORD dwLastError)
 {
-    dbj_string_512 msg_ = {{""}};
+    // dbj_string_512 msg_ = {{""}};
     HMODULE hModule = NULL; // default to system source
-    LPSTR MessageBuffer;
-    DWORD dwBufferLength;
+    dbj_string_512 MessageBuffer = {{0}};
+    DWORD dwBufferLength = sizeof MessageBuffer.data;
 
-    DWORD dwFormatFlags = FORMAT_MESSAGE_ALLOCATE_BUFFER |
-                          FORMAT_MESSAGE_IGNORE_INSERTS |
-                          FORMAT_MESSAGE_FROM_SYSTEM;
+    DWORD dwFormatFlags = /* FORMAT_MESSAGE_ALLOCATE_BUFFER | */ FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_SYSTEM;
 
     //
     // If dwLastError is in the network range,
-    //  load the message source.
+    //  load the message source dll.
+    // if not already loaded
     //
     if (dwLastError >= NERR_BASE && dwLastError <= MAX_NERR)
     {
@@ -47,8 +46,7 @@ DisplayErrorText(
                 NULL,
                 LOAD_LIBRARY_AS_DATAFILE);
         }
-
-        if (netmsg_handle_ != NULL)
+        else //    netmsg_handle_ != NULL
         {
             dwFormatFlags |= FORMAT_MESSAGE_FROM_HMODULE;
             hModule = netmsg_handle_;
@@ -66,64 +64,55 @@ DisplayErrorText(
              hModule, // module to get message from (NULL == system)
              dwLastError,
              MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // default language
-             (LPSTR)&MessageBuffer,
-             0,
+             (LPSTR)&MessageBuffer.data,
+             sizeof(MessageBuffer.data) - 1,
              NULL)))
     {
         if (also_dump_to_stderr_)
         {
-            dbj_string_512 final_buffer_ = {{0}};
+            static dbj_string_512 final_buffer_ = {{0}};
 
-#if 1
-            static_assert(_countof(final_buffer_.data) == sizeof(final_buffer_.data), "count and sizeof");
+            DBJ_STRING_SET_EMPTY(final_buffer_);
+
+#ifdef _DEBUG
+            static_assert(_countof(final_buffer_.data) == sizeof(final_buffer_.data), "count == sizeof");
 #endif
 
-            // C11 --> int snprintf_s(char *restrict buffer, rsize_t bufsz, const char *restrict format, ...);
+            // how much has been written into the final_buffer
             int snprintf_rez = _snprintf_s((char *restrict)final_buffer_.data,
-                                           (rsize_t)sizeof(final_buffer_.data) - 1,
+                                           (rsize_t)sizeof(final_buffer_.data),
                                            _TRUNCATE,
-                                           (const char *restrict)"[%4d]: %s", dwLastError, MessageBuffer);
+                                           (const char *restrict)"[%6d]: %s", dwLastError, MessageBuffer);
 
-            DBJ_ASSERT(snprintf_rez > 0);
+            if (snprintf_rez < 0)
+                goto clean_exit;
 
             // message to stderr.
             DWORD dwBytesWritten = 0;
-            // no error checking, yes I know, I know ...
-            // Roadmap: send to dbj_syslog.dll ;)
-            BOOL write_file_rez = WriteFile(
+            // so simple it is difficult to spot: WriteFile does not know about zero delimited strings
+            /*BOOL write_file_rez =*/(void)WriteFile(
                 GetStdHandle(STD_ERROR_HANDLE),
                 final_buffer_.data,
                 snprintf_rez, /* write only this much */
                 &dwBytesWritten,
                 NULL);
 
-            DBJ_ASSERT(write_file_rez);
-        }
-
-        // basically nobody knows what is the win err max message length
-        // maybe it is 512 since that is a BUFSIZ, me thinks
-        DBJ_ASSERT(dwBufferLength < 512);
-
-        DBJ_STRING_ASSIGN(msg_, MessageBuffer);
-
-        //
-        // Free the buffer allocated by the system.
-        //
-        LocalFree(MessageBuffer);
+            // DBJ_ASSERT(write_file_rez);
+        } // also_dump_to_stderr_
     }
 
     //
-    // If we loaded a message source, unload it.
-    // DBJ: no leave it in, OS will take care of it on exit
+    // DBJ: leave the dll in, OS will take care of it on exit
     // if (hModule != NULL)
     //    FreeLibrary(hModule);
 
-    return msg_;
+clean_exit:
+    return MessageBuffer;
 }
 static dbj_string_512 error_message_implementation_(struct component_syserrmsg *self_, const unsigned error_code)
 {
     DBJ_ASSERT(self_);
-    return DisplayErrorText(error_code);
+    return MakeErrorText(error_code);
 }
 /* --------------------------------------------------------------------------------- */
 static void also_to_stderr_implementation(bool arg_)
